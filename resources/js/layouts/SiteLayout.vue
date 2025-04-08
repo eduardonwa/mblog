f0d55a02a75699c7c0ef7388b18eb2e82489c352
Eduardo Coello <eduardongua@hotmail.com>
2025-04-07 21:10:45 -0700

    cambie el login/register al SiteLayout y deje Welcome para empezar a crear el index de la pagina

--- resources/js/layouts/SiteLayout.vue
+++ resources/js/layouts/SiteLayout.vue
@@ -1,23 +1,37 @@
 <script setup>
-    import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3'; // ¡Asegúrate de importar Link!
 </script>
 
 <template>
-    <div>
-        <Head>
-            <title v-if="$page.props.meta?.title">{{ $page.props.meta.title }}</title>
-            <title v-else>s1ck0fm3t4L</title>
-            <meta name="description" content="Descripción general del sitio">
-            <meta name="csrf-token" :content="$page.props.csrf_token">
-            <link rel="icon" href="/favicon.ico">
-        </Head>
+  <div>
+    <Head>
+      <title v-if="$page.props.meta?.title">{{ $page.props.meta.title }}</title>
+      <title v-else>s1ck0fm3t4L</title>
+      <meta name="description" content="Descripción general del sitio">
+      <meta name="csrf-token" :content="$page.props.csrf_token">
+      <link rel="icon" href="/favicon.ico">
+      <!-- Agrega las fuentes de Inter aquí para que estén disponibles globalmente -->
+      <link rel="preconnect" href="https://rsms.me/" />
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
 
-        <header v-if="$page.props.auth.user">
-            <!-- navbar -->
-        </header>
+    <!-- Header movido desde Welcome.vue -->
+    <header>         
+      <nav>
+        <!-- aqui pongo enlaces normales -->
+        <Link v-if="$page.props.auth.user" :href="route('dashboard')">
+          Dashboard
+        </Link>
         
-        <main>
-            <slot />
-        </main>
-    </div>
+        <template v-else>
+          <Link :href="route('login')">Log in</Link>
+          <Link :href="route('register')">Register</Link>
+        </template>
+      </nav>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+  </div>
 </template>
--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,36 +1,15 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
+import SiteLayout from '@/layouts/SiteLayout.vue';
 </script>
 
 <template>
-    <Head title="Welcome">
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
-    <div>
-        <header>
-            <nav>
-                <Link
-                    v-if="$page.props.auth.user"
-                    :href="route('dashboard')"
-                >
-                    Dashboard
-                </Link>
-                <template v-else>
-                    <Link
-                        :href="route('login')"
-                    >
-                        Log in
-                    </Link>
-                    <Link
-                        :href="route('register')"
-                    >
-                        Register
-                    </Link>
-                </template>
-            </nav>
-        </header>
-    </div>
-
+  <SiteLayout>
+    <Head title="Welcome" />
     
+    <div>
+      <h1>¡Bienvenido a mi sitio!</h1>
+
+    </div>
+  </SiteLayout>
 </template>
