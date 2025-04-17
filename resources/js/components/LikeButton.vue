5513d48f7b9e57ee49191ffcd5fedb63e8235c99
Eduardo Coello <eduardongua@hotmail.com>
2025-04-16 18:08:41 -0700

    agregue la animación pero no se ve bien

--- resources/js/components/LikeButton.vue
+++ resources/js/components/LikeButton.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   import axios from 'axios';
+  import { ref } from 'vue';
 
   const props = defineProps({
       post: {
@@ -9,11 +10,17 @@
   });
 
   const emit = defineEmits(['update:post']);
+  const isPulsing = ref(false);
+  const isCounting = ref(false);
 
   const toggleLike = async (e: MouseEvent) => {
     e.preventDefault();
     const wasLiked = props.post.is_liked_by_user;
     
+    // activar animaciones
+    isPulsing.value = true;
+    isCounting.value = true;
+
     // Optimistic update
     const tempPost = {
         ...props.post,
@@ -43,24 +50,34 @@
         // Rollback en caso de error
         emit('update:post', props.post);
         console.error('Error:', error);
+    } finally {
+      setTimeout(() => {
+        isPulsing.value = false;
+        isCounting.value = false;
+      })
     }
 };
 </script>
 
 <template>
-  <div>
+  <div class="like-button-wrapper">
     <button
       @click="toggleLike($event)"
       :class="{
         'like-button--active': post.is_liked_by_user,
-        'like-button--inactive': !post.is_liked_by_user
+        'like-button--inactive': !post.is_liked_by_user,
+        'pulse-animation': isPulsing
       }"
       :aria-label="post.is_liked_by_user ? 'Unlike' : 'Like'"
+      class="button"
+      data-type="like"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+      <div class="like-counter" :class="{'animating': isCounting}">
+        <span class="like-count">{{ post.likes_count }}</span>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="30" height="30" viewBox="0 0 34 34">
+        <path d="M24 6c-1.7 0-3 1.3-3 3v2.2c-.3-.1-.6-.2-1-.2-.8 0-1.5.3-2 .8-.5-.5-1.2-.8-2-.8-.4 0-.7.1-1 .2V7c0-1.7-1.3-3-3-3S9 5.3 9 7v10.9c-.5-.5-1.2-.9-2-.9-.9 0-1.7.3-2.3.9-1.1 1.1-1.2 3-.1 4.2l3.2 3.6C9.6 27.8 12.2 29 15 29h3.7c4.6 0 8.4-3.8 8.4-8.4V9C27 7.3 25.7 6 24 6zm1 10v4.6c0 3.5-2.9 6.4-6.4 6.4h-3.7c-2.2 0-4.2-.9-5.7-2.6L6 20.8c-.4-.4-.4-1.1.1-1.5.2-.2.5-.3.8-.3.3 0 .6.2.8.4l1.5 1.8c.3.3.7.4 1.1.3.4-.1.7-.5.7-.9V7c0-.6.4-1 1-1s1 .4 1 1v8c0 .6.4 1 1 1s1-.4 1-1v-1c0-.6.4-1 1-1s1 .4 1 1v1c0 .6.4 1 1 1s1-.4 1-1v-1c0-.6.4-1 1-1s1 .4 1 1v1c0 .6.4 1 1 1s1-.4 1-1V9c0-.6.4-1 1-1s1 .4 1 1v7z"/>
       </svg>
-      <span>{{ post.likes_count }}</span>
     </button>
   </div>
 </template>
