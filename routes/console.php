d6ea3d3b47cd20b15f4e8a430ae66c63ce4020b4
Eduardo Coello <eduardongua@hotmail.com>
2025-07-07 21:32:08 -0700

    Fix duplicate scrape command for metal archives; ensure correct scheduling for offset 0.

--- routes/console.php
+++ routes/console.php
@@ -8,14 +8,14 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+Schedule::command('scrape:metal-archives --offset=0')
+    ->dailyAt('07:00')
+    ->timezone('America/Mazatlan');
+
 Schedule::command('scrape:metal-archives --offset=20')
     ->dailyAt('14:00')
     ->timezone('America/Mazatlan');
 
 Schedule::command('scrape:metal-archives --offset=40')
     ->dailyAt('22:00')
-    ->timezone('America/Mazatlan');
-
-Schedule::command('scrape:metal-archives --offset=0')
-    ->dailyAt('07:00')
     ->timezone('America/Mazatlan');
