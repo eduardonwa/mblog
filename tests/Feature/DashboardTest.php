b3b04e77196c4b0e5ed3075eaa72319b253aef14
Eduardo Coello <eduardongua@hotmail.com>
2025-07-28 11:42:50 -0700

    comente algunos tests que ya no aplican para la aplicacion

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -10,11 +10,11 @@ class DashboardTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_guests_are_redirected_to_the_login_page()
+    /* public function test_guests_are_redirected_to_the_login_page()
     {
         $response = $this->get('/');
         $response->assertRedirect('/login');
-    }
+    } */
 
     public function test_authenticated_users_can_visit_the_dashboard()
     {
--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -21,7 +21,7 @@ class ProfileUpdateTest extends TestCase
         $response->assertOk();
     }
 
-    public function test_profile_information_can_be_updated()
+/*     public function test_profile_information_can_be_updated()
     {
         $user = User::factory()->create();
 
@@ -41,9 +41,9 @@ class ProfileUpdateTest extends TestCase
         $this->assertSame('Test User', $user->username);
         $this->assertSame('test@example.com', $user->email);
         $this->assertNull($user->email_verified_at);
-    }
+    } */
 
-    public function test_email_verification_status_is_unchanged_when_the_email_address_is_unchanged()
+/*     public function test_email_verification_status_is_unchanged_when_the_email_address_is_unchanged()
     {
         $user = User::factory()->create();
 
@@ -59,9 +59,9 @@ class ProfileUpdateTest extends TestCase
             ->assertRedirect('/settings/profile');
 
         $this->assertNotNull($user->refresh()->email_verified_at);
-    }
+    } */
 
-    public function test_user_can_delete_their_account()
+/*     public function test_user_can_delete_their_account()
     {
         $user = User::factory()->create();
 
@@ -77,7 +77,7 @@ class ProfileUpdateTest extends TestCase
 
         $this->assertGuest();
         $this->assertNull($user->fresh());
-    }
+    } */
 
     public function test_correct_password_must_be_provided_to_delete_account()
     {
